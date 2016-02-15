e89504d0813c1b8bc86b938f486950d95894e7eb
fishthezombie <fishthezombie@sharklasers.com>
2016-02-16 03:24:40 +0700

    hello

---
+++ Assets/CubeController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+    GameObject playerCube;
+
+	// Use this for initialization
+	void Start () {
+        playerCube = new GameObject();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
