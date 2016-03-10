8e983cef7658764cf6da106e34888efb7b9538f4
themetalbacon <ageisalie@gmail.com>
2016-03-10 14:48:43 -0600

    merge

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    void Update()
+    {
+        //Move the bullet by speed based on local axis
+        transform.position += transform.up * Time.deltaTime * 5f;
+    }
+
+    void OnTriggerEnter2D (Collider2D collider)
+    {
+        if (collider.CompareTag("PlayerBullet"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/GameArea.cs
+++ Assets/Scripts/GameArea.cs
@@ -2,12 +2,12 @@
 using System.Collections;
 
 public class GameArea : MonoBehaviour {
-	
-	//Detect collider's trigger
+
+    //Detect collider's trigger
     void OnTriggerExit2D(Collider2D checkObject)
     {
         //Check if the object is tagged as "PlayerBullet"
-        if (checkObject.CompareTag("PlayerBullet"))
+        if (checkObject.CompareTag("PlayerBullet") || checkObject.CompareTag("Enemy"))
         {
             //Destroy object
             Destroy(checkObject.gameObject);
